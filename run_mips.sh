c7e6822f34bdb5084c90a2e183967a784fae73c2
blcslk <115918743+blcslk@users.noreply.github.com>
2022-10-16 21:30:42 +0200

    Update run_mips.sh

--- run_mips.sh
+++ run_mips.sh
@@ -12,12 +12,12 @@ screen --help >/dev/null 2>&1 || apt install qemu-system-mips screen -y >/dev/nu
 
 echo " "
 [ "$1" = "y" ] || [ "$1" = "-y" ] || read -p "Full run (y) or background '&' task? (y/n) " RUN1
-[ "$1" = "y" ] && RUN1='y'
+[ "$1" = "y" ] &&  RUN1='y'
 [ "$1" = "-y" ] && RUN1='y'
 if [ "$RUN1" = "y" ]; then
 #[ `ps aux | grep debian.*.mips | sed -n '2p' | wc -l` = '1' ] && kill "$(ps aux | grep debian.*.mips | awk '{print $2}' | sed -n '1p')"
 #[ `pidof linux_mips | wc -l` = '0' ] || || kill `pidof linux_mips`
 qemu-system-mips -M malta -kernel vmlinux-3.2.0-4-4kc-malta -hda debian_wheezy_mips_standard.qcow2 -append "root=/dev/sda1" -nographic -nic user,hostfwd=tcp::2222-:22 || echo -e 'process run problem, use -> kill `pidof qemu-system-mips`'
 else
-qemu-system-mips -M malta -kernel vmlinux-3.2.0-4-4kc-malta -hda debian_wheezy_mips_standard.qcow2 -append "root=/dev/sda1" -nographic -nic user,hostfwd=tcp::2222-:22 &
+qemu-system-mips -M malta -kernel vmlinux-3.2.0-4-4kc-malta -hda debian_wheezy_mips_standard.qcow2 -append "root=/dev/sda1" -nographic -nic user,hostfwd=tcp::2222-:22 >/dev/null 2>&1 &
 fi
